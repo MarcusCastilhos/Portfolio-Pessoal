d3fbaf079651ee6ab7ef0ec8cb12b27dfa4f8ed2
Marcus Castilhos <marcus.drummer@live.com>
2025-02-06 02:10:29 -0300

    feat: added slide components, font awesome module and adjust styles

---
+++ src/app/features/about/description/about-slide2/about-slide2.component.ts
@@ -0,0 +1,38 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-slide2',
+  imports: [],
+  template: `
+    <div class="slide-content2">
+      <div class="img-description fadeIn">
+        <img src="/marcus.jpg" alt="Marcus" />
+      </div>
+      <div class="side-description fadeIn">
+        <h2 class="title">Desenvolvedor Web Full-Stack</h2>
+        <p>
+          &nbsp;&nbsp;Sou um profissional apaixonado por tecnologia e inovação,
+          sempre em busca de novos desafios para crescer e evoluir. Com uma
+          trajetória marcada por disciplina e dedicação, venho de uma
+          experiência única no Exército, onde aprimorei habilidades valiosas
+          como liderança, responsabilidade e a capacidade de aprender
+          rapidamente. Esses valores são aplicados no meu trabalho, onde busco
+          sempre entregar soluções eficazes e criativas.
+        </p>
+        <p>
+          &nbsp;&nbsp;Estou em uma jornada contínua de desenvolvimento e estou
+          pronto para contribuir com meu potencial em projetos que me desafiem a
+          aprender ainda mais. Fique à vontade para explorar meu portfólio e
+          conhecer mais sobre minhas experiências e projetos. Se você procura
+          alguém comprometido, responsável e com uma grande capacidade de
+          adaptação, estou à disposição para conversar sobre como posso agregar
+          valor à sua equipe.
+        </p>
+        <p>Vamos conversar!</p>
+      </div>
+    </div>
+  `,
+  styleUrl: './about-slide2.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AboutSlide2Component {}
